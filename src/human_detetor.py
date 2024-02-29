d2305f2638dca8d3508ab24a7ece62d5186de148
Larinenok <olegins03@gmail.com>
2024-02-29 15:54:36 +0700

    Update human_detector

--- src/human_detetor.py
+++ src/human_detetor.py
@@ -2,31 +2,34 @@ import cv2
 from cvzone.PoseModule import PoseDetector
 
 
-detector = PoseDetector()
-original_image = cv2.imread('images/people/2.jpg')
-image = original_image.copy()
+def get_humans() -> list:
+    detector = PoseDetector()
+    original_image = cv2.imread('images/people/2.jpg')
+    image = original_image.copy()
 
-all_boxes = []
+    all_boxes = []
 
-while True:
-    image = detector.findPose(img=image, draw=False)
-    _, boxes = detector.findPosition(img=image, draw=False)
+    while True:
+        image = detector.findPose(img=image, draw=False)
+        _, boxes = detector.findPosition(img=image, draw=False)
 
-    if (boxes == {}):
-        break
+        if (boxes == {}):
+            break
 
-    bbox = boxes['bbox']
-    all_boxes.append(bbox)
+        bbox = boxes['bbox']
+        all_boxes.append(bbox)
 
-    pos1 = (bbox[0] + int(bbox[2] / 10), bbox[1])
-    pos2 = (bbox[0] + bbox[2] - int(bbox[2] / 10), bbox[1] + bbox[3])
+        pos1 = (bbox[0] + int(bbox[2] / 10), bbox[1])
+        pos2 = (bbox[0] + bbox[2] - int(bbox[2] / 10), bbox[1] + bbox[3])
 
-    cv2.rectangle(image, pos1, pos2, (0, 0, 0), -1)
+        cv2.rectangle(image, pos1, pos2, (0, 0, 0), -1)
 
-for bbox in all_boxes:
-    pos1 = (bbox[0], bbox[1])
-    pos2 = (bbox[0] + bbox[2], bbox[1] + bbox[3])
+    for bbox in all_boxes:
+        pos1 = (bbox[0], bbox[1])
+        pos2 = (bbox[0] + bbox[2], bbox[1] + bbox[3])
 
-    cv2.rectangle(original_image, pos1, pos2, (0, 255, 0))
+        cv2.rectangle(original_image, pos1, pos2, (0, 255, 0))
 
-cv2.imwrite('images/output/output.jpg', img=original_image)
+    cv2.imwrite('images/output/output.jpg', img=original_image)
+
+    return all_boxes
